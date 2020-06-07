1aee61baa6d85240accb4b6f1ad81b7ab995c3df
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-06-07 22:03:08 +0700

    redo

--- applications/controllers/ExamController.js
+++ applications/controllers/ExamController.js
@@ -371,8 +371,7 @@ exports.sendUserSequence = async (req, res) => {
     var user = await User.findById(req.user._id)
     var metas = JSON.stringify(user.metas)
     metas = JSON.parse(metas)
-    metas.sequences = req.body.sequences
-    metas.seqActive = req.body.seqActive
+    metas.sequences = req.body
     var userUpdate = await User.findOneAndUpdate({
         _id: req.user._id,
     },{

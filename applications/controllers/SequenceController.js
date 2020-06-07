addebf3944ace4c0b45d8541c0ae47e630afc08a
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-06-07 22:45:29 +0700

    debug sequence

--- applications/controllers/SequenceController.js
+++ applications/controllers/SequenceController.js
@@ -66,6 +66,10 @@ exports.view = function (req, res) {
 
 // Handle update user info
 exports.update = function (req, res) {
+    res.json({
+        oke:'oke'
+    })
+    return
     Sequence.findById(req.params.sequence_id,  async function (err, sequence) {
         if (err)
         {
